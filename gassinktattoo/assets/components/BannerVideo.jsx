b0b3530bb6d0f47dc366be76b609fdfcb9f25963
JuanGassinDAW <juangaming97@gmail.com>
2024-05-29 21:44:20 +0200

    Añadido banner de video

--- gassinktattoo/assets/components/BannerVideo.jsx
+++ gassinktattoo/assets/components/BannerVideo.jsx
@@ -5,7 +5,7 @@ const BannerVideo = () => {
     return (
         <div className="video-banner">
             <video autoPlay loop muted className="banner-video">
-                <source src='/videos/bannervideo.mp4' type="video/mp4" />
+                <source src='./videos/bannervideo.mp4' type="video/mp4" />
             </video>
         </div>
     );
